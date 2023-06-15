29c13bc4d4ff5c50f679f9abe83dd38b1096feeb
Cristina-tmp <cristinacommissions@gmail.com>
2023-06-15 01:30:03 -0400

    Completed login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./components/Home/HomeScreen";
-
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/Login/LoginPage";
+import { app } from "../../firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 function App() {
   console.log("hi");
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+    // cleanup
+    return unsubscribe;
+  }, []);
   return (
     <>
       <div className="app">
@@ -12,7 +37,18 @@ function App() {
         </header>
 
         <main>
-          <HomeScreen />
+          <Routes>
+            {!user ? (
+              <>
+                <Route path="/" element={<LoginPage />} />
+              </>
+            ) : (
+              <>
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/" element={<HomeScreen />} />
+              </>
+            )}
+          </Routes>
         </main>
       </div>
     </>
